448db175b26d467dcd1855293f288f56b4d4fd70
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-20 00:04:01 -0400

    Updates logic to tally a winner

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,39 +12,39 @@ function playRound(playerSelection, computerSelection) {
         case 'rock':
             switch (computerSelection) {
                 case 'rock':
-                    result = 'It\'s a tie!';
+                    result = 1;
                     break;
                 case 'paper':
-                    result = 'You lose! Paper beats rock';
+                    result = 0;
                 break;
                 case 'scissors':
-                    result = 'You win! Rock beats paper';
+                    result = 2;
                 break;
             }
         break;
         case 'paper':
             switch (computerSelection) {
                 case 'rock':
-                    result = 'You win! Paper beats rock';
+                    result = 2;
                     break;
                 case 'paper':
-                    result = 'It\'s a tie!';
+                    result = 1;
                 break;
                 case 'scissors':
-                    result = 'You lose! Scissors beats paper';
+                    result = 1;
                 break;
             }
         break;
         case 'scissors':
             switch (computerSelection) {
                 case 'rock':
-                    result = 'You lose! Rock beats scissors';
+                    result = 0;
                     break;
                 case 'paper':
-                    result = 'You win! Scissors neats paper';
+                    result = 2;
                 break;
                 case 'scissors':
-                    result = 'It\'s a tie!';
+                    result = 1;
                 break;
             }
         break;
@@ -54,9 +54,41 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    let playerWinCount = 0;
+    let computerWinCount = 0;
+    let message = '';
+
     for (let i = 0; i < 5; i++)
     {
         let playerChoice = prompt('Rock, paper, or scissors?');
-        console.log(playRound(playerChoice, getComputerChoice()));
+        let computerChoice = getComputerChoice();
+
+        switch (playRound(playerChoice, computerChoice)) {
+            case 0:
+                ++computerWinCount;
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                break;
+            case 1:
+                console.log (`It's a tie!`);
+                break;
+            case 2:
+                ++playerWinCount;
+                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                break;
+        }
     }
+
+    if (playerWinCount == computerWinCount)
+    {
+        message = `It's a tie!`;
+    }
+    else if (playerWinCount > computerWinCount)
+    {
+        message = `Player wins!`;
+    }
+    else {
+        message = `Computer wins!`;
+    }
+
+    console.log(`Game over! The player won ${playerWinCount} times and the computer won ${computerWinCount} times! ${message}`)
 }
