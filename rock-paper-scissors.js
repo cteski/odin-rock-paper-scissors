5269b6e44b6e0f6d021306dee8f320987e4d08fb
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-19 23:18:32 -0400

    Add playRound function to js
    Noting to self this is janky and should be refactored

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,3 +3,52 @@ function getComputerChoice() {
     choices.push('rock', 'paper', 'scissors');
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    let result = '';
+    let loweredPlayerSelection = playerSelection.toLowerCase();
+
+    switch (loweredPlayerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'rock':
+                    result = 'It\'s a tie!';
+                    break;
+                case 'paper':
+                    result = 'You lose! Paper beats rock';
+                break;
+                case 'scissors':
+                    result = 'You win! Rock beats paper';
+                break;
+            }
+        break;
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    result = 'You win! Paper beats rock';
+                    break;
+                case 'paper':
+                    result = 'It\'s a tie!';
+                break;
+                case 'scissors':
+                    result = 'You lose! Scissors beats paper';
+                break;
+            }
+        break;
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock':
+                    result = 'You lose! Rock beats scissors';
+                    break;
+                case 'paper':
+                    result = 'You win! Scissors neats paper';
+                break;
+                case 'scissors':
+                    result = 'It\'s a tie!';
+                break;
+            }
+        break;
+    }
+
+    return result;
+}
